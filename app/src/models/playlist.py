84ae1e81ed9fb10a3f172eb74c8d14ff24ca2036
giftedodd <giftedoddd@gmail.com>
2024-11-30 21:39:57 +0300

    Updates on audioPlayer.py

--- app/src/models/playlist.py
+++ app/src/models/playlist.py
@@ -1,36 +1,57 @@
 import random as rd
 
 class Playlist:
-    """PlayList class for manage queue and player behaviors."""
+    """
+    PlayList class for manage queue and player behaviors.
+    """
     def __init__(self):
-        self.songs = []
-        self.current_index = 0
+        self.songs = []             # List of current songs in queue.
+        self.current_index = 0      # Current index of the list pointer.
 
     def add_song(self, song_object:object) -> None:
-        """Gets a song object as argument per call to add it to the queue."""
+        """
+        Gets a song object as argument per call to add it to the queue.\n
+        Args: Song object
+        Returns: none
+        """
         self.songs.append(song_object)
 
     def play(self) -> object:
-        """Returns a song object of current position."""
+        """
+        Returns a song object of current position.\n
+        Args: none
+        Returns Song object
+        """
         if self.songs:
             return self.songs[self.current_index]
 
     def next(self) -> object:
-        """Increase index position by 1 and return song object of that position."""
+        """
+        Increase index position by 1 and return song object of that position.\n
+        Args: none
+        Returns: Song object
+        """
         if self.songs:
             self.current_index = (self.current_index + 1) % len(self.songs)
             return self.play()
 
     def previous(self) -> object:
-        """Decrease index position by 1 and return song object of that position."""
+        """
+        Decrease index position by 1 and return song object of that position.\n
+        Args: none
+        Returns: Song object
+        """
         if self.songs:
             self.current_index = (self.current_index - 1) % len(self.songs)
             return self.play()
-        return None
 
     def shuffle(self) -> None:
-        """Shuffles the queue from current position index to the last, objects before current index will be at same
-         position as before"""
+        """
+        Shuffles the queue from current position index to the last, objects before current index will be at same
+        position as before.\n
+        Args: none
+        Returns: none
+         """
         played = self.songs[:self.current_index + 1:]
         in_queue = self.songs[self.current_index + 1::]
         rd.shuffle(in_queue)
