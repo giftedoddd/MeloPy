4b86d937dbed5b7dea74e729d7958dc1f0ec27be
giftedodd <giftedoddd@gmail.com>
2024-12-30 20:33:12 +0300

    adding icons

--- MeloPy/src/interface/interface.py
+++ MeloPy/src/interface/interface.py
@@ -1,3 +1,5 @@
+from PIL import ImageTk
+import PIL.Image
 from pathlib import Path
 from tkinter import *
 
@@ -8,11 +10,11 @@ ASSETS_PATH = Path(__file__).parent.parent.parent.joinpath("assets/")
 class Interface(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.slider = None
         self.static_modules = []
         self.lower_modules = []
         self.max_height, self.max_width = self.winfo_screenheight(), self.winfo_screenwidth()
         self.base_init()
-        # self.widgets_()
         self.lower_widgets()
 
     def base_init(self):
@@ -24,24 +26,69 @@ class Interface(Tk):
         background = "#353535"
 
         previous_icon = ASSETS_PATH.joinpath("previous-50.png")
-        self.static_modules.append(PhotoImage(master=self, file=previous_icon))
+        self.static_modules.append(PhotoImage(file=previous_icon))
 
         play_icon = ASSETS_PATH.joinpath("play-50.png")
-        self.static_modules.append(PhotoImage(master=self, file=play_icon))
+        self.static_modules.append(PhotoImage(file=play_icon))
 
         next_icon = ASSETS_PATH.joinpath("next-50.png")
-        self.static_modules.append(PhotoImage(master=self, file=next_icon))
+        self.static_modules.append(PhotoImage(file=next_icon))
 
-        frame = Frame(master=self, background=background, border=0, height=self.max_height // 10, width=self.max_width)
-        frame.place(rely=0.9)
+        image_path = ASSETS_PATH.joinpath("lana.jpg").open("rb")
+        image = PIL.Image.open(image_path)
+        image = image.resize((125, 125))
+        self.static_modules.append(ImageTk.PhotoImage(image))
 
-        previous_button = Button(master=frame, background=background, image=self.static_modules[0], border=0, highlightthickness=False)
+        singer_icon = ASSETS_PATH.joinpath("singer-25.png")
+        self.static_modules.append(PhotoImage(file=singer_icon))
+
+        album_icon = ASSETS_PATH.joinpath("album-25.png")
+        self.static_modules.append(PhotoImage(file=album_icon))
+
+        song_icon = ASSETS_PATH.joinpath("song-25.png")
+        self.static_modules.append(PhotoImage(file=song_icon))
+
+        frame = Frame(master=self, background=background, border=0, height=self.max_height // 8, width=self.max_width)
+        frame.place(rely=0.88)
+
+        previous_button = Button(master=frame, background=background, image=self.static_modules[0], border=0,
+                                 highlightthickness=False)
         previous_button.place(relx=0.45, rely=0.6)
 
-        play_button = Button(master=frame, background=background, image=self.static_modules[1], border=0, highlightthickness=False)
+        play_button = Button(master=frame, background=background, image=self.static_modules[1], border=0,
+                             highlightthickness=False)
         play_button.place(relx=0.48, rely=0.6)
 
-        next_button = Button(master=frame, background=background, image=self.static_modules[2], border=0, highlightthickness=False)
+        next_button = Button(master=frame, background=background, image=self.static_modules[2], border=0,
+                             highlightthickness=False)
         next_button.place(relx=0.51, rely=0.6)
+
+        artist_image = Label(master=frame, image=self.static_modules[3])
+        artist_image.place(relx=0.004, rely=0.35)
+
+        singer_name = Label(master=frame, text="Lana Del Rey", font=("Arial", 15), background=background,
+                            foreground=FOREGROUND)
+        singer_name.place(relx=0.065, rely=0.35)
+
+        singer_image = Label(master=frame, image=self.static_modules[4], background=background)
+        singer_image.place(relx=0.053, rely=0.39)
+
+        album_name = Label(master=frame, text="Lust For Life", font=("Arial", 15), background=background, foreground=FOREGROUND)
+        album_name.place(relx=0.065, rely=0.55)
+
+        album_image = Label(master=frame, image=self.static_modules[5], background=background)
+        album_image.place(relx=0.053, rely=0.58)
+
+        song_name = Label(master=frame, text="Ultraviolence", font=("Arial", 15), background=background,
+                          foreground=FOREGROUND)
+        song_name.place(relx=0.065, rely=0.75)
+
+        song_image = Label(master=frame, image=self.static_modules[6], background=background)
+        song_image.place(relx=0.053, rely=0.77)
+
+        self.slider = Scale(master=frame, border=5, orient=HORIZONTAL, from_=1, to=500, cursor="arrow", sliderrelief=FLAT,
+                            background=background, highlightthickness=False, relief=FLAT, sliderlength=1, length=self.max_width - 40,
+                            width=20)
+        self.slider.place(relx=0.004, rely=0.0)
 
         self.lower_modules.append(frame)
